7d0178d09a2c99995e3ad03595e69fa6d53018ee
Sebastian Gonzalez M <sebagonz91@gmail.com>
2014-07-08 18:11:00 -0400

    It needs to implement a smart backtracking

--- gale_shapley.cpp
+++ gale_shapley.cpp
@@ -20,6 +20,14 @@ int to_int( const string &Text )//Text not by const reference so that the functi
   return ss >> result ? result : 0;
 }
 
+//Deletes a specefied value on a vector
+void deleteElement(vector<int>& vector, int personId)
+{
+  std::vector<int>::iterator position = find(vector.begin(), vector.end(), personId);
+  if (position != vector.end()) // == vector.end() means the element was not found
+      vector.erase(position);
+}
+
 string printPrefList(PrefList list){
   string result = "[ ";
   if(list.size() == 1)
@@ -51,7 +59,7 @@ string printVectorPrefList(vector<PrefList> list){
 void printMarriages(Couples matching){
   for (Couples::const_iterator it = matching.begin(); it != matching.end(); ++it)
   {
-      cout << "\t" << it->first << " and " << it->second << "\n";
+      cout << "\t" << it->second << " and " << it->first << "\n";
   }
 }
 
@@ -153,6 +161,26 @@ bool prefersCurrent(PrefList girlList, int husband, int newGuy){
   }
 }
 
+bool matchingStable(Couples matching, vector<PrefList> menPrefs, vector<PrefList> womenPrefs)
+{
+  for (auto it = matching.cbegin(); it != matching.cend() ; ++it)
+  {
+    int bride = (*it).first;
+    int herGroom = (*it).second;
+    for (auto newGuyIt = matching.cbegin(); newGuyIt != matching.cend(); ++newGuyIt)
+    {
+      int someGuy = (*newGuyIt).second;
+      int hisWife = (*newGuyIt).first;
+
+      if (!prefersCurrent(womenPrefs[bride - 1], herGroom, someGuy) && !prefersCurrent(menPrefs[someGuy - 1], hisWife, bride))
+      {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 //Forces node consistency on the preferences lists, that is, if some person p of sex A has an empty
 //preference list, then p is deleted from all the preferences list of people from sex B
 void nodeConsistency(vector< PrefList >& menPrefs, vector< PrefList>& womenPrefs)
@@ -220,52 +248,96 @@ Couples galeShapley(queue<int>* bachelors, vector<PrefList> menPrefsIn, vector<P
 
 void forwardChecking(vector<PrefList> menPrefs, vector<PrefList> womenPrefs)
 {
-  vector<int> bachelors;
-  vector<int> girls;
+  vector<int> originBachelors;
+  vector<int> originSingleGirls;
   int next = 0;
-  int loopsNumber = menPrefs.size();
+  int numberOfGirls;
 
   for (int i = 0; i < menPrefs.size(); i++)
   {
-    bachelors.push_back(i+1);
+    originBachelors.push_back(i+1);
   }
   for (int i = 0; i < womenPrefs.size(); i++)
   {
-    girls.push_back(i+1);
+    originSingleGirls.push_back(i+1);
   }
 
   Couples fCheck;
-  int girl = girls.front();
-  int boy = bachelors.front();
-  fCheck[girl] = boy;
 
-  //Se borran los elegidos de la lista
-  girls.erase(girls.begin());
-  bachelors.erase(bachelors.begin());
-  while(loopsNumber != 0) {
-    girl = girls.front();
-    boy = bachelors.front();
+  for (int i = 0; i < originSingleGirls.size(); i++)
+  {
+    fCheck.clear();
+    //Realizar mismo procedimiento para cada posible pareja del primer hombre
+    //Ej
+    // 1 -> 1
+    // 1 -> 2
+    // 1 -> 3
+    // 1 -> 4
+    vector<int> bachelors = originBachelors;
+    vector<int> singleGirls = originSingleGirls;
+    
+    int boy = bachelors.front();
+    int girl = singleGirls[i];
+    int loopsNumber = bachelors.size();
+    //Primero asociamos la pareja
+    cout << "chico " << boy << " con chica " << girl << endl;
+    fCheck[girl] = boy;
+    cout << "tamaño de matching: " << fCheck.size() << endl;
+    numberOfGirls = womenPrefs.size();
 
-    cout << "chicas ";
-    printVector(girls);
-    cout << "solteros ";
-    printVector(bachelors);
+    //DEBEN EXISTIR LISTA DE PREFERENCIAS GLOBALES, INMUTABLES
+    //AL ELIMINAR A UN HOMBRE DE LISTA SOLTEROS Y MUJER DE LISTA DE SOLTEROS,
+    //HAY QUE BORRARLOS DE LAS LISTAS DE PREFERENCIAS TEMPORALES (EXISTEN SOLO PARA LA ACTUAL ITERACION)
 
-    girls.erase(girls.begin());
-    bachelors.erase(bachelors.begin());
-    if (girls.empty() or bachelors.empty())
-    {
-      cout << "chicas ";
-      printVector(girls);
-      cout << "solteros ";
-      printVector(bachelors);
-      break;
+    //Se borran los elegidos de la lista
+    deleteElement(singleGirls, girl);
+    deleteElement(bachelors, boy);
+    while(loopsNumber >= 0) {
+      int girl = singleGirls.front();
+      int boy = bachelors.front();
+      cout << "Buscando mina para el hombre " << boy << endl;
+      //Hacerlos pareja y revisar estabilidad
+      fCheck[girl] = boy;
+      cout << "chico " << boy << " con chica " << girl << endl;
+      while(!matchingStable(fCheck, menPrefs, womenPrefs)) {
+        //Si es inestable, hay que deshacer la ultima pareja
+        //Intentar emparejar a boy con la siguiente soltera (sin eliminar a girl de la lista de solteras)
+        cout << "...MATCHING INESTABLE" << endl;
+        fCheck.erase(girl);
+        next += 1;
+        if (next == numberOfGirls - 1)
+        {
+          cout << "No hay mas chicas para intentar" << endl;
+          break;
+        }
+        girl = singleGirls[next];
+        fCheck[girl] = boy;
+        cout << "chico " << boy << " con chica " << girl << endl;
+      }
+      cout << "...MATCHING ESTABLE" << endl;
+      //La pareja hace el match estable
+      //Se mantienen casados, pero se borran de la lista de solteros
+      deleteElement(singleGirls, girl);
+      deleteElement(bachelors, boy);
+
+      // cout << "chicas ";
+      // printVector(singleGirls);
+      // cout << "solteros ";
+      // printVector(bachelors);
+      if (singleGirls.empty() or bachelors.empty())
+      {
+        break;
+      }
+      loopsNumber--;
     }
-    loopsNumber--;
+    cout << "Matrimonios estables:" << endl;
+    printMarriages(fCheck);
+
+    cout << "===========================" << endl;
   }
-  printMarriages(fCheck);
+  
   // cout << "chicas ";
-  // printVector(girls);
+  // printVector(singleGirls);
   // cout << "solteros ";
   // printVector(bachelors);
   // Matchings marriages;
@@ -279,29 +351,22 @@ int main(int argc, char const *argv[])
   vector <PrefList> menPrefs;
   vector <PrefList> womenPrefs;
   queue<int> bachelors;
-  queue<int> sluts;
   Couples couples;
 
   prefs = loadData(argv[1]);
   for (int i = 0; i < prefs.size()/2; i++)
   {
     menPrefs.push_back(prefs[i]);
-    //Fill the bachelors and the sluts
+    //Fill the bachelors
     bachelors.push(i+1);
-    sluts.push(i+1);
   }
   for (int i = prefs.size()/2; i < prefs.size(); i++)
   {
     womenPrefs.push_back(prefs[i]);
   }
 
-  // cout << "Hombres" << endl;
-  // cout << printVectorPrefList(menPrefs) << endl;
-  // cout << "Mujeres" << endl;
-  // cout << printVectorPrefList(womenPrefs) << endl;
   forwardChecking(menPrefs, womenPrefs);
   // couples = galeShapley(&bachelors, menPrefs, womenPrefs);
-
   // printMarriages(couples);
   // cout << couples.size() << endl;
   cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << " seconds" << endl;
